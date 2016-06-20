dc7a1d07d6185eb09abc8565d99a2464cd9d19a5
Ahmad Kandil <kandileng@gmail.com>
2016-06-20 16:05:35 +0200

    N. Forecast

--- Codeforces Solutions/630N.cpp
+++ Codeforces Solutions/630N.cpp
@@ -14,11 +14,11 @@ int main()
 			r1 = (-b + sqrt(s)) /(2 * a);
 			r2 = (-b - sqrt(s)) / (2 * a);
 			if(r2>r1)
-            {
-                s=r2;
-                r2=r1;
-                r1=s;
-            }
+                        {
+                            s=r2;
+                            r2=r1;
+                            r1=s;
+                        }
 			cout <<fixed << setprecision(16) << r1 << endl;
 			cout <<fixed << setprecision(16) << r2 << endl;
 		}
