129bd1031043c99ac5bf0fdb6d14116df2f5972c
Ahmad Kandil <kandileng@gmail.com>
2016-07-19 14:44:00 +0200

    B. Barnicle

--- Codeforces Solutions/697.cpp
+++ Codeforces Solutions/697.cpp
@@ -6,11 +6,18 @@ typedef long long L;
 typedef int I;
 typedef string S;
 
+L power(L b,L s)
+{
+    if(s==0)
+        return 1;
+    return b*power(b,s-1);
+}
+
 I to_decimal(S ms)
 {
     I res=0,j;
     for(j=0;j<ms.length();j++)
-        res += (ms[j]-48)*pow(10,ms.length()-j-1);
+        res += (ms[j]-48)*power(10,ms.length()-j-1);
 
     return res;
 }
@@ -44,11 +51,13 @@ int main()
     real = v.substr(0,dot);
     dec = v.substr(dot+1,e-dot-1);
     for(I j=dec.length()-1;j>=0;j--)
-      if(dec[j]!=48||j==0)
+      if(dec[j]!=48)
       {
           dec = dec.substr(0,j+1);
           break;
       }
+      else if(j==0)
+          dec = "";
     ms = v.substr(e+1,v.length()-e-1);
 
     vms = to_decimal(ms);
@@ -58,6 +67,6 @@ int main()
     else
         res = real+dec.substr(0,vms)+'.'+dec.substr(vms,dec.length()-vms);
 
-    cout<<res<<'\n';
+    cout<<res<<endl;
     return 0;
 }
