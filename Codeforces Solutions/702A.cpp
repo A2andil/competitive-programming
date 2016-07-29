f04b2e0b683e1af1153b572979341b8cc2623c12
Ahmad Kandil <kandileng@gmail.com>
2016-07-29 17:51:08 +0200

    A. Maximum Increase

---
+++ Codeforces Solutions/702A.cpp
@@ -0,0 +1,28 @@
+//In The Name of Allah
+#include <iostream>
+
+using namespace std;
+
+int arr[100005];
+int main()
+{
+    int n,max,i,j,count;
+    cin>>n;
+    for(i=0;i<n;++i) cin>>arr[i];
+
+    max=1;
+    for(i=0;i<n;++i){
+        count = 1;
+        for(j=i+1;j<n;++j){
+            if(arr[j-1]<arr[j]) count++;
+            if(arr[j-1]>arr[j]||j==n-1){
+                max = (max<count)?count:max;
+                i =j-1;
+                break;
+            }
+
+        }
+    }
+    cout<<max<<endl;
+    return 0;
+}
