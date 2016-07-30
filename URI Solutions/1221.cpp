f52e7e7b4a45e6da702bb8f68c6893c45a6254a8
Ahmad Kandil <kandileng@gmail.com>
2016-07-31 00:36:18 +0200

    URI Solutions

--- URI Solutions/1221.cpp
+++ URI Solutions/1221.cpp
@@ -3,30 +3,18 @@
 using namespace std;
 bool prime(int y)
 {
-	if(y==1||y==2||y==3)
-	return true;
-	else
-	{
-		for(int p=sqrt(y);p>=2;p--)
-		{
-			if(y%p==0)
-			return false;
-			else if(y%p!=0&&p==2)
-			return true;
+	if(y==1||y==2||y==3) return true;
+		for(int p=sqrt(y);p>=2;p--){
+			if(y%p==0) return false;
+			else if(y%p!=0&&p==2) return true;
 		}
-	}
 }
-int main ()
-{
-  int n,y;
-  cin>>n;
-  for(int i=0;i<n;i++)
-  {
-  	cin>>y;
-  	 if(prime(y))
-  	 cout<<"Prime"<<endl;
-  	 else
-  	 cout<<"Not Prime"<<endl;
+int main (){
+  int n,y; cin>>n;
+  for(int i=0;i<n;i++){
+  	 cin>>y;
+  	 if(prime(y)) cout<<"Prime"<<endl;
+  	 else cout<<"Not Prime"<<endl;
   }
   
   return 0;
