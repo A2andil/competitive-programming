e06a48851092c8bc718bdeffc52ce7003c1df3be
Ahmad Kandil <kandileng@gmail.com>
2016-08-19 01:28:45 +0200

    URI Solutions

---
+++ URI Solutions/2172.cpp
@@ -0,0 +1,12 @@
+//In The Name of Allah
+#include <iostream>
+#define ll long long
+using namespace std;
+
+int main(){
+    ll n,m;
+    while(cin>>n>>m&&n&&m){
+        cout<<n*m<<endl;
+    }
+    return 0;
+}
