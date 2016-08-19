502ce721b2f561d54eac7958267605fe9c1c9763
Ahmad Kandil <kandileng@gmail.com>
2016-08-19 02:37:11 +0200

    URI Solutions

---
+++ URI Solutions/2162.cpp
@@ -0,0 +1,24 @@
+//In The Name of Allah
+#include <iostream>
+#define ll long long
+using namespace std;
+int peaks[105],cnt[105]={0};
+int main(){
+    int n; cin>>n>>peaks[1];
+    for(int i=2;i<=n;i++){
+        cin>>peaks[i];
+        if(peaks[i]>peaks[i-1])
+            cnt[i]=1;
+        else if(peaks[i]<peaks[i-1])
+            cnt[i]=0;
+        else cnt[i]=-1;
+    }
+    bool chck=true;
+    for(int i=2;i<=n;i++)
+        if((cnt[i]==-1)||(i+1<=n&&cnt[i]==cnt[i+1])){
+            chck=false;
+            break;
+        }
+    cout<<(chck?1:0)<<endl;
+    return 0;
+}
