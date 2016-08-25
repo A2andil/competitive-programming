48b0740bfe27f908cfff2aae4b958ea80db6c6bb
Ahmad Kandil <kandileng@gmail.com>
2016-08-25 23:03:23 +0200

    A. Dubstep

--- Codeforces Solutions/208A.cpp
+++ Codeforces Solutions/208A.cpp
@@ -1,22 +1,18 @@
+//In The Name of Allah
 #include <iostream>
-
+#define EV ""
 using namespace std;
 
-int main()
-{
-    string v,res="";
-    cin>>v;
-    for(int i=0;i<v.length();i++)
-    {
-        if(i+2<v.length()&&v[i]=='W'&&v[i+1]=='U'&&v[i+2]=='B')
-        {
-            if(res!=""&&res[res.length()-1]!=' ')
+int main() {
+    string v,res=EV; cin>>v;
+    for(int i=0;i<v.length();i++) {
+        if(i+2<v.length()&&v[i]=='W'&&v[i+1]=='U'&&v[i+2]=='B') {
+            if(res!=EV&&res[res.length()-1]!=' ')
                res+=' ';
 
             i+=2;
         }
-        else
-            res += v[i];
+        else res += v[i];
     }
     cout<<res<<endl;
     return 0;
